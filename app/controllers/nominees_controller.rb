c7647a86883fe3e0a1242f93e7b59481b817c61d
asuthamm <asuthamm@gmail.com>
2020-03-10 13:22:32 -0400

    remove obsolete folder

--- app/controllers/nominees_controller.rb
+++ app/controllers/nominees_controller.rb
@@ -10,4 +10,31 @@ class NomineesController < ApplicationController
         render json: @nominee
     end
 
+    def create
+        @nominee = Nominee.create(nominee_params)
+        if @nominee.valid?
+          render json: @nominee, status: 201
+        else
+          render json: @nominee.errors.full_messages, status: 422
+        end
+    end
+    
+    def update
+        @nominee = Nominee.find(params[:id])
+        @nominee.update(nominee_params)
+        render json: @nominee
+      end
+    
+      def destroy
+          @nominee = Nominee.find(params[:id])
+          @nominee.destroy
+          render json: {message: "Nominee is now deleted", nominee: @nominee}
+      end
+
+
+      
+    private
+    def nominee_params
+        params.permit(:name, :recipient)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -53,43 +53,56 @@
 # ]
 # Award.create(awards)
 
-shows = [
-  {
-    "title": "",
-    "show_type": "",
-    "opening_date": "",
-    "closing_date": '',
-    "performances": 0,
-    "likes": 0,
-    "image": ""
-  },
-  {
-    "title": "",
-    "show_type": "",
-    "opening_date": "",
-    "closing_date": '',
-    "performances": 0,
-    "likes": 0,
-    "image": ""
-  },
-  {
-    "title": "",
-    "show_type": "",
-    "opening_date": "",
-    "closing_date": '',
-    "performances": 0,
-    "likes": 0,
-    "image": ""
-  },
-  {
-    "title": "",
-    "show_type": "",
-    "opening_date": "",
-    "closing_date": '',
-    "performances": 0,
-    "likes": 0,
-    "image": ""
-  }
-]
-Show.create(shows)
+# shows = [
+#   {
+#     "title": "The Lion King",
+#     "show_type": "Musical",
+#     "opening_date": "November 13, 1997",
+#     "closing_date": '',
+#     "performances": 9243,
+#     "likes": 0,
+#     "image": "https://upload.wikimedia.org/wikipedia/en/5/58/The_Lion_King_Musical.svg"
+#   },
+#   {
+#     "title": "Cats",
+#     "show_type": "Musical",
+#     "opening_date": "October 7, 1982",
+#     "closing_date": '',
+#     "performances": 7485,
+#     "likes": 0,
+#     "image": "https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/CatsMusicalLogo.jpg/220px-CatsMusicalLogo.jpg"
+#   },
+#   {
+#     "title": "Wicked",
+#     "show_type": "Musical",
+#     "opening_date": "October 30, 2003",
+#     "closing_date": '',
+#     "performances": 6777,
+#     "likes": 0,
+#     "image": "https://upload.wikimedia.org/wikipedia/en/b/b4/Wicked-poster.jpg"
+#   },
+#   {
+#     "title": "Les Misérables",
+#     "show_type": "Music",
+#     "opening_date": "March 12, 1987",
+#     "closing_date": '',
+#     "performances": 6680,
+#     "likes": 0,
+#     "image": "https://images-na.ssl-images-amazon.com/images/I/51zqFCjSuTL._AC_.jpg"
+#   }
+# ]
+# Show.create(shows)
 
+# awards = [
+#   {
+#     "award_ceremony": "1988 Tony Award",
+#     "category": "Best Performance by a Featured Actress in a Musical",
+#     "show_id": 1
+#   },
+#   {
+#     "award_ceremony": "1988 Tony Award",
+#     "category": "Best Direction of a Musical",
+#     "show_id": 1
+#   },
+# ]
+# Award.create(awards)
